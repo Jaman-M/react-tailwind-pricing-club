4c23dbbe29d4f9ec48c557e21897d839917297dd
Jaman-M <jaman.manik1@gmail.comm>
2022-04-03 17:37:49 +0600

    added hero icon and xIcon

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from '../Link/Link';
-
+import { MenuIcon, XIcon } from '@heroicons/react/solid'
 const Navbar = () => {
+    const [open, setOpen] = useState(false)
     const routes = [
         {id:1, name:'Home',link: '/home'},
         {id:2, name:'Shop',link: '/shop'},
@@ -11,7 +12,10 @@ const Navbar = () => {
     ]
     return (
         <nav>
-            <ul>
+            <div onClick={()=>setOpen(!open)} className='w-6 h-6 md:hidden'>
+                {open ? <XIcon></XIcon> : <MenuIcon></MenuIcon>}
+            </div>
+            <ul className='md:flex justify-center'>
                 {
                     routes.map(route => <Link 
                         key={route.id}
